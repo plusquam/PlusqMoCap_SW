0229aa1755e1b03355f4fedf34394b643eb5ab33
plusquam <swierk.damian@gmail.com>
2019-09-12 00:25:43 +0200

    Turn off magnetometer

--- Inc/app_mpu.h
+++ Inc/app_mpu.h
@@ -23,7 +23,7 @@
 #endif
 
 // Sensors settings
-#define MPU_SENSORS_SET 	(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS)
+#define MPU_SENSORS_SET 	(INV_XYZ_GYRO | INV_XYZ_ACCEL) //| INV_XYZ_COMPASS)
 #define MPU_ACCEL_FSR		4 	// +/- 4 G
 #define MPU_GYRO_FSR		500	// +/- 500 dps
 #define MPU_DLPF_BAND		98 	// 98 Hz
